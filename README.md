40909ad5222d1e297916ede937f413373aae0213
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-10-08 12:24:48 +0530

    feat(init): initialized app

---
+++ README.md
@@ -0,0 +1,19 @@
+# Un-Hook
+
+<div style="text-align:center">
+    <img src="./assets/img/logo-black.png" style="width: 200px" />
+</div>
+
+If you are one of the rare breed of people who call themselves programmers, you must have faced the following sitation:
+
+You were so busy working, that you forgot to take a break while coding... now your eyes hurt due to the excessive stress on them.
+
+The solution? Use Un-Hook. An app that reminds you to take breaks at the right time.
+
+## How to Use
+
+1. Download and go to the repository location.
+2. Install depenencies for main app using `npm install`
+3. Go to front-end folder using `cd front-end` and use `npm install` to install front end realted dependencies.
+4. Use `npm run start-front-end` to start the react app
+5. Use `npm run dev` to start the electron app
---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+let mainWindow = null
+const isMac = process.platform === 'darwin' ? true : false
+const isWin = process.platform === 'win32' ? true : false
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 450,
+        icon: path.join(__dirname, "assets", "img", "logo-white.png"),
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    mainWindow.loadURL('http://localhost:3000/')
+}
+
+app.on('ready', createMainWindow)
+
+app.on('window-all-closed', () => {
+    if (isMac) {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createMainWindow()
+    }
+})
+
+if (isWin) {
+    app.setAppUserModelId(app.name)
+}
